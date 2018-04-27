0def6bd574d2c83018da3c35914a5973044cdb80
virgil23 <virgil17@live.com>
2018-04-27 19:50:49 -0400

    that I finished the html and added some logic for the day

---
+++ assets/javascript/app.js
@@ -0,0 +1,213 @@
+// Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyA7gB0HYSZ2LuPRNsczmvpIbhk6rcw25_s",
+    authDomain: "week-7-bf49d.firebaseapp.com",
+    databaseURL: "https://week-7-bf49d.firebaseio.com",
+    projectId: "week-7-bf49d",
+    storageBucket: "week-7-bf49d.appspot.com",
+    messagingSenderId: "457724217194"
+  };
+  firebase.initializeApp(config);
+
+
+// Variables 
+
+var database = firebase.database();
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+//                    Real Code                 ///
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+// initial variables 
+var name = "";  // Train name
+var dest = "";  // Destination
+var firstTime = ""; // First train time in HH:mm
+var freq = 0;   // Frenquency in minutes until the next arrival
+var away = 0; // Minutes away 
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+// When you click submit... This happens...
+$("#submit").on("click", function(event) {
+    // Don't refresh the page!
+    event.preventDefault();
+
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+    /////////////////////////////////////////////
+
+// Grabs value from these fields, will do firstTime separate as we have to calculate first
+    nameInput = $("#name-input").val().trim();
+    destInput = $("#dest-input").val().trim();
+    freqInput = $("#freq-input").val().trim();
+
+
+    // capture the first train time input in moment.js
+ //////////////////////////////////////////////////////
+
+var randomTime = $("#first-input").val().trim();
+var randomFormat = "HH:mm";
+var convertedDate = moment(randomTime, randomFormat); 
+
+
+// Converts military time that user inputs, into civilian time
+// We put that calculated time into a variable called "firstTime"
+var firstTime = moment(convertedDate).format('hh:mm');
+// Then we log it
+console.log(firstTime);
+console.log("..................................................");
+
+// Variable that will calculate the next train arrival will be here !!!
+var nextArrival = 
+    
+    
+
+// Puts info on the firebase database
+    database.ref().set({
+      name: nameInput,
+      dest: destInput,
+      firstTime: firstTime,
+      freq: freqInput
+    });
+
+    
+    // Grabs defined variables, and adds them to the Board
+    $("#input").append(
+        '<tr><td>'
+        +nameInput+
+        '</td><td>'
+        +destInput+
+        '</td><td>'
+        +freqInput+
+        '</td><td>'
+        +nextArrival+
+        '</td>'
+    );
+
+    clearInput();
+
+  });
+
+
+
+  // Clears the input field after hitting submit 
+  var clearInput = function() {
+    $("#name-input").val("");
+    $("#dest-input").val("");
+    $("#first-input").val("");
+    $("#freq-input").val("");
+  }
+////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+
+// This displays to the dom when you submit
+
+// Firebase watcher + initial loader HINT: .on("value")
+database.ref().on("value", function(snapshot) {
+
+    // Log everything that's coming out of snapshot
+    console.log(snapshot.val());
+    console.log(snapshot.val().nameInput);
+    console.log(snapshot.val().destInput);
+    console.log(snapshot.val().nextArrival);
+    console.log(snapshot.val().freqInput);
+
+
+    /*
+    // Change the HTML to reflect
+    $("#name-display").text(snapshot.val().name);
+    $("#email-display").text(snapshot.val().email);
+    $("#age-display").text(snapshot.val().age);
+    $("#comment-display").text(snapshot.val().comment);
+    */
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+
+
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+//                    Test Code                 ///
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+ // Initializing our click count at 0
+ var clickCounter = 0;
+
+ // Functions
+ // ================================================================================
+
+ // On Click
+ $("#click-button").on("click", function() {
+
+   // Add 1 to clickCounter
+   clickCounter++;
+
+   // **** Store Click Data to Firebase in a JSON property called clickCount *****
+   // **** Note how we are using the Firebase .set() method ****
+   // **** .ref() refers to the path you want to save your data to
+   // **** Since we left .ref() blank, it will save to the root directory
+   database.ref().set({
+     clickCount: clickCounter
+   });
+
+   // Now! go to https://fir-click-counter-7cdb9.firebaseio.com/ to see the impact to the DB
+ });
+
+
+
+ ////////////////////////////////////////////////////////////
+ //////////////////////////////////////////////////////////////
+ // end of test
+
+
+/*var start = '2018-04-30';
+
+var result = moment(start).fromNow();
+
+console.log(result);
+
+/////////////////////////////////////////////////
+
+var end = '2018-05-25';
+
+var result2 = moment(end).diff(moment(start));
+
+console.log(result2);
+
+// This works, but displays it in miliseconds
+///////////////////////////////////////////////
+// humanize turns miliseconds into days
+var humanize = moment.duration(result2).humanize();
+console.log(humanize);
+
+// You can display it as hours, days, or minutes
+///////////////////////////////////////////////////
+// as hours
+var asHours = moment.duration(result2).as('hours');
+console.log(asHours);
+//as minutes
+var asMinutes = moment.duration(result2).as('minutes');
+console.log(asMinutes);
+// as days
+var asDays = moment.duration(result2).as('days');
+console.log(asDays);
+
+////////////////////////
+// displays from military time to civilian time
+var time = moment().format('hh:mm:ss');
+console.log(time); */
